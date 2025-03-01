581bf653718ed21c1d5bc7725ec14c1093bc12a7
isylllane <Lutik301@gmail.com>
2025-03-01 21:28:55 +0300

    init

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
-require('dotenv').config();
-const express = require('express');
-const sequelize = require('./db');
+require("dotenv").config();
+const express = require("express");
+const sequelize = require("./db");
 const PORT = process.env.PORT || 7000;
-const models = require('./models/models');
-const router = require('./routes/index');
-const cors = require('cors');
-const fileUpload = require('express-fileupload');
-const path = require('path');
+const models = require("./models/models");
+const router = require("./routes/index");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const path = require("path");
 
-const errorHandler = require('./middleware/ErrorHandingMiddleware');
+const errorHandler = require("./middleware/ErrorHandingMiddleware");
 
 const app = express();
 app.use(cors());
-app.use(express.static(path.resolve(__dirname, 'static')));
+app.use("/api/img", express.static(path.join(__dirname, "static")));
 app.use(express.json());
 app.use(fileUpload({}));
 
-app.use('/api', router);
+app.use("/api", router);
 
 // Обработка ошибок
 app.use(errorHandler);
 
 const start = async () => {
-    try {
-        await sequelize.authenticate()
-        await sequelize.sync()
-        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
-    } catch (error) {
-        console.log(error)
-    }
-}
-start()
-
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
